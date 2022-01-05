31d60e3feeb591b9796593b5f79fb013f5d492e4
Amr Desouky <desoukya@gmail.com>
2022-01-05 11:13:42 +0200

    init | remove mongoose and use mongodb instead | parallelize http calls

---
+++ db.js
@@ -0,0 +1,34 @@
+
+   
+const { MongoClient, ObjectID } = require('mongodb');
+const bluebird = require('bluebird');
+
+bluebird.promisifyAll(MongoClient);
+
+const MONGO_URI = 'mongodb://localhost:27017'
+const MONGO_DB_NAME = 'sc';
+let dbConnection;
+
+const connect = async () => {
+  try {
+    const client = await MongoClient.connect(MONGO_URI);
+    dbConnection = client.db(MONGO_DB_NAME);
+  } catch (e) {
+    throw new Error(`Could not establish database connection: ${e}`);
+  }
+};
+
+const mongoClient = async collectionName => {
+  if (!dbConnection) {
+    await connect();
+  }
+  if (collectionName) {
+    return dbConnection.collection(collectionName);
+  }
+  return dbConnection;
+};
+
+module.exports = {
+  mongoClient,
+  ObjectID
+};
